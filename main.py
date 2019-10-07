c2a3a5aca4b0065750c2d1fde4a04bf287527215
Vits-Rimer Anna <vitsrimer.anna@gmail.com>
2019-10-07 22:57:11 +0300

    Add start server to WebController. Add start function in main.py.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+"""
+
+
+Для поддержки нескольких пользователей нужно дополнительно на js
+    написать сохранение access token
+"""
+
+
 from Instagram import Instagram
 from ApplicationSettings import application_settings
 from SqliteDataStorage import SqliteDataStorage
-from InstagramItem import InstagramItem
+import WebController
 
-access_token = application_settings.token
-api = Instagram(access_token)
-# print(api.media())
-db = SqliteDataStorage(application_settings.db_name)
-data = db.get_photos(application_settings.token)
-print(InstagramItem.retrieve_list(data))
+
+def start():
+    access_token = application_settings.token
+    api = Instagram(access_token)
+    # print(api.media())
+    db = SqliteDataStorage(application_settings.db_name)
+    data = db.get_photos(application_settings.token)
+    # print(InstagramItem.retrieve_list(data))
+    WebController.start_server(
+        application_settings.web_host,
+        application_settings.web_port,
+        application_settings.templates_path)
+
+
+if __name__ == '__main__':
+    start()
