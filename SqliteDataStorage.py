0c8dbde6cbe9782a466f3f93f0ab0faaaade1b21
Vits-Rimer Anna <vitsrimer.anna@gmail.com>
2019-10-07 23:16:58 +0300

    Added func insert_photo in SqliteDataStorage.

--- SqliteDataStorage.py
+++ SqliteDataStorage.py
@@ -5,7 +5,12 @@ import sqlite3
 
 class SqliteDataStorage:
     def __init__(self, db_path):
-        self._connection = sqlite3.connect(db_path)
+        self._connection = None
+        try:
+            self._connection = sqlite3.connect(db_path)
+        except Exception as e:
+            print(e)
+
         self._cursor = self._connection.cursor()
 
     def get_photos(self, token):
@@ -16,8 +21,11 @@ class SqliteDataStorage:
         result = self._cursor.fetchall()
         return result
 
-    def insert_photo(self):
-        pass
+    def insert_photo(self, photo_id, photo_link, user_id):
+        query = fr'''INSERT INTO instagram_items(photo_id, photo_link, user_id) +
+        VALUES("{photo_id}", "{photo_link}", "{user_id}")'''
+        self._cursor.execute(query)
+        return self._cursor.lastrowid
 
     def remove_photo(self):
         pass
