d9cf753d7bf33a365cab3b9c30e402f09711e2e2
Vits-Rimer Anna <vitsrimer.anna@gmail.com>
2019-10-08 21:44:43 +0300

    Added created time to db.

--- InstagramItem.py
+++ InstagramItem.py
@@ -6,6 +6,7 @@ class InstagramItem:
     photo_id: str
     photo_link: str
     user_id: str
+    created_time: str
 
     def __eq__(self, other):
         if self.photo_id == other.photo_id:
@@ -19,16 +20,16 @@ class InstagramItem:
         for item in list:
             if isinstance(item, dict):
                 result.append(InstagramItem(item['id'], item['images']['low_resolution']['url'],
-                                            item['user']['id']))
+                                            item['user']['id'], item['created_time']))
             else:
-                result.append(InstagramItem(item[0], item[1], item[2]))
+                result.append(InstagramItem(item[0], item[1], item[2], item[3]))
         return result
 
     @staticmethod
     def compare_and_save_to_db(media_data, db_data, db):
         for media_item in media_data:
             if media_item not in db_data:
-                db.insert_photo(media_item.photo_id, media_item.photo_link, media_item.user_id)
+                db.insert_photo(media_item.photo_id, media_item.photo_link, media_item.user_id, media_item.created_time)
 
     def convert_to_dict(self):
         return asdict(self)
--- SqliteDataStorage.py
+++ SqliteDataStorage.py
@@ -12,17 +12,18 @@ class SqliteDataStorage:
         self._cursor = self._connection.cursor()
 
     def get_photos(self, token):
-        query = fr'''SELECT instagram_items.photo_id, instagram_items.photo_link, instagram_items.user_id FROM 
-        instagram_items INNER JOIN clients ON instagram_items.user_id = clients.user_id WHERE 
-        clients.access_token = "{token}" '''
+        query = fr'''SELECT instagram_items.photo_id, instagram_items.photo_link, instagram_items.user_id, 
+        instagram_items.created_time FROM instagram_items INNER JOIN clients ON 
+        instagram_items.user_id = clients.user_id WHERE clients.access_token = "{token}" '''
 
         self._cursor.execute(query)
         result = self._cursor.fetchall()
         return result
 
-    def insert_photo(self, photo_id, photo_link, user_id):
+    def insert_photo(self, photo_id, photo_link, user_id, created_time):
         try:
-            query = fr'''INSERT INTO instagram_items(photo_id,photo_link,user_id) VALUES("{photo_id}","{photo_link}","{user_id}")'''
+            query = fr'''INSERT INTO instagram_items(photo_id, photo_link, user_id, created_time) 
+            VALUES("{photo_id}", "{photo_link}", "{user_id}", "{created_time}")'''
             self._cursor.execute(query)
         except sqlite3.IntegrityError as e:
             print(e)
