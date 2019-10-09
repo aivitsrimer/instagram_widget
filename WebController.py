5ea666b0c47a8535990bd86b9ff5c90a20dd5e52
Vits-Rimer Anna <vitsrimer.anna@gmail.com>
2019-10-09 18:54:49 +0300

    Fixed bug with catching error. Refactor update db logic. Added global api and db in Web.

--- WebController.py
+++ WebController.py
@@ -10,8 +10,13 @@ from Instagram import Instagram
 from ApplicationSettings import application_settings
 from SqliteDataStorage import SqliteDataStorage
 
+from UpdateDataStorage import update_db
+
 
 routes = web.RouteTableDef()
+
+__api: Instagram = None
+__db: SqliteDataStorage = None
 
 
 @routes.get('/')
@@ -24,37 +29,46 @@ async def main_page(request: web.Request):
 @aiohttp_jinja2.template('widget.jinja2')
 async def widget(request: web.Request):
     name = 'Photos'
-    api = Instagram(application_settings.token)
-    db = SqliteDataStorage(application_settings.db_name)
-    data = db.get_photos(application_settings.token, application_settings.widget_photo_limit)
+    update_db(__api, __db)
+    if __api.error_msg:
+        return {'name': name, 'photos': None, 'error': __api.error_msg}
+
+    data = __db.get_photos(application_settings.token, application_settings.widget_photo_limit)
     photos = InstagramItem.retrieve_list(data)
 
-    return {'name': name, 'photos': photos, 'error': api.error_msg}
+    return {'name': name, 'photos': photos, 'error': __api.error_msg}
 
 
 @routes.get('/api/media')
 async def api_media(request: web.Request) -> web.Response:
-    api = Instagram(application_settings.token)
-    return web.json_response(api.media())
-    # or api.error_msg
+    if __api.error_msg:
+        return web.json_response(_json_error(__api.error_msg))
+    return web.json_response(__api.media())
 
 
 @routes.get('/api/widget')
 async def api_widget(request: web.Request) -> web.Response:
-    api = Instagram(application_settings.token)
-    # media = api.media()
-    db = SqliteDataStorage(application_settings.db_name)
-    data = db.get_photos(application_settings.token, application_settings.widget_photo_limit)
+    update_db(__api, __db)
+    if __api.error_msg:
+        return web.json_response(_json_error(__api.error_msg))
+
+    data = __db.get_photos(application_settings.token, application_settings.widget_photo_limit)
     photos = InstagramItem.retrieve_list(data)
 
     response = []
     for photo in photos:
         response.append(photo.convert_to_dict())
     return web.json_response(response)
-    # or api.error_msg
 
 
-def start_server(host, port, templates_path):
+def _json_error(error_msg):
+    return dict(error=str(error_msg))
+
+
+def start_server(host, port, templates_path, api, db):
+    global __api, __db
+    __api = api
+    __db = db
 
     templates_directory = Path(__file__).parent.joinpath(templates_path)
     app = web.Application()
@@ -65,4 +79,7 @@ def start_server(host, port, templates_path):
 
 
 if __name__ == '__main__':
-    start_server('127.0.0.1', int('8080'), 'templates/')
+    access_token = application_settings.token
+    api = Instagram(access_token)
+    db = SqliteDataStorage(application_settings.db_name)
+    start_server('127.0.0.1', int('8080'), 'templates/', api, db)
--- main.py
+++ main.py
@@ -13,30 +13,22 @@ from Instagram import Instagram
 from ApplicationSettings import application_settings
 from SqliteDataStorage import SqliteDataStorage
 from InstagramItem import InstagramItem
+from UpdateDataStorage import update_db
 import WebController
 
 
 def start():
     access_token = application_settings.token
     api = Instagram(access_token)
-    media = api.media()
-
-    if not media:
-        print(api.error_msg)
-    else:
-        media_data = InstagramItem.retrieve_list(media)
-        return media_data
-
     db = SqliteDataStorage(application_settings.db_name)
-    data = db.get_photos(access_token, application_settings.widget_photo_limit)
-    db_data = InstagramItem.retrieve_list(data)
 
-    InstagramItem.compare_and_save_to_db(media_data, db_data, db)
+    update_db(api, db)
 
     WebController.start_server(
         application_settings.web_host,
         application_settings.web_port,
-        application_settings.templates_path)
+        application_settings.templates_path,
+        api, db)
 
 
 if __name__ == '__main__':
